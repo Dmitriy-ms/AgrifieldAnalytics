9f9bb9210e15dc06869b75ec6e3d5feba7f7f849
Dmitriy-ms <granqq@yandex.ru>
2024-04-05 10:26:18 +0400

    Add class Field

---
+++ src/main/java/com/agrifieldanalytics/AgriFieldAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.agrifieldanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriFieldAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriFieldAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agrifieldanalytics/AgriFieldAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrifieldanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriFieldAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
