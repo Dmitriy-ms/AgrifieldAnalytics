cfc61d2e956ecc5f3d5d48b9058405b0655b0fb3
Dmitriy-ms <granqq@yandex.ru>
2024-04-05 14:13:21 +0400

    add class Sort

---
+++ src/main/java/com/agrifieldanalytics/model/Sort.java
@@ -0,0 +1,15 @@
+package com.agrifieldanalytics.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Sort {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Long cultureId;
+}
