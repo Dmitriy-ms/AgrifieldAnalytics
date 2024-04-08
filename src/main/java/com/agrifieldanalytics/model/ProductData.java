c8eb43d369b8ecdde25e3419b5fbcfa1e99c6fe2
Dmitriy-ms <granqq@yandex.ru>
2024-04-08 11:47:34 +0400

    add class ProductData

---
+++ src/main/java/com/agrifieldanalytics/model/ProductData.java
@@ -0,0 +1,41 @@
+package com.agrifieldanalytics.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class ProductData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "seeds_sown")
+    private String seedsSown;
+    @Column(name = "price_seeds")
+    private String priceSeeds;
+    @Column(name = "expenses_fuel")
+    private String expensesFuel;
+    @Column(name = "price_fuel")
+    private String priceFuel;
+    @Column(name = "other_expenses")
+    private String otherExpenses;
+    @Column(name = "total_cost")
+    private String totalCost;
+    @Column(name = "produced_products")
+    private String producedProducts;
+    @Column(name = "crop_yield")
+    private String cropYield;
+    @Column(name = "crop_price")
+    private String cropPrice;
+    @Column(name = "sold")
+    private String sold;
+    @Column(name = "revenue")
+    private String revenue;
+    @Column(name = "sale_sales")
+    private String saleSales;
+    @Column(name = "date_harvest")
+    private String dateHarvest;
+    @Column(name = "date_sale")
+    private String dateSale;
+
+}
